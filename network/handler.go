23eb09cd46ace442103e4b6c4373fac20ef01654
tobz <tobias.lawrence@gmail.com>
2014-01-31 09:14:39 -0500

    Removing cruft.

--- network/handler.go
+++ network/handler.go
@@ -1,23 +1,6 @@
 package network
 
+import "github.com/tobz/phosphorus/constants"
 import "github.com/tobz/phosphorus/interfaces"
-
-type BasePacketHandler struct {
-    packetType PacketType
-    packetCode uint64
-}
-
-func (bh *BasePacketHandler) GetPacketType() PacketType {
-    return bh.packetType
-}
-
-func (bh *BasePacketHandler) GetPacketCode() uint64 {
-    return bh.packetCode
-}
-
-type BasicPacketHandler interface {
-    GetPacketType() PacketType
-    GetPacketCode() uint64
-}
 
 type InboundPacketHandler func(client interfaces.Client, packet *InboundPacket) error
